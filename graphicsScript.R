c0e939ef6d8ad6f6e5d179d787e371c109da9d1f
Luis Fuentes Fuentes <80366974+L4F7@users.noreply.github.com>
2024-04-06 15:14:38 -0600

    Add generate graphics code

--- graphicsScript.R
+++ graphicsScript.R
@@ -1,57 +1,25 @@
-#1 Conocer el % de variancia explicada para determinar cuántos componentes utilizar 
-#para calcular los puntajes Leverage
 
-var_exp
+# Read CSV file
+data <- read.csv("Dataset.csv")
 
+plot(data$Age.at.enrollment, data$Admission.grade, 
+     xlab = "Edad de matrícula", ylab = "Grado de admisión",
+     main = "Gráfico de dispersión de la edad de matrícula frente al grado de admisión")
 
-var_exp(Dataset, standardize = TRUE, Marital.status:Course)
-# Se escogió como rango la autoeficacia hasta la nota de bachillerato para un 
-#total de 182 variables.
+# Crea un vector con los nombres de las categorías del eje x
+nombres_x <- c("Soltero", "Casado", "Viudo", "Divorciado", "Unión de hecho", "Legalmente separados")
 
+# Crea el gráfico de barras y especifica los nombres de las categorías en el eje x
+barplot(table(data$Marital.status), 
+        cex.main = 1, 
+        col = "chocolate", 
+        main = "Distribución de estudiantes según estado civil", 
+        xlab = "Estado civil", 
+        ylab = "Cantidad de estudiantes", 
+        xlim = c(0, 7), 
+        ylim = c(0, 4000), 
+        sub = "Fuente: Elaboración propia",
+        names.arg = nombres_x)
 
-#2 #Classic CUR with top scores selection criteria.
-result <- CUR(data=Dataset, variables=Marital.status:Course,
-              k=20, rows = 1, columns = .2, standardize = TRUE,
-              cur_method = "sample_cur")
-result
-
-#Como resultado de este leverage_columns
-#1         11.777423   SalGasColCabDep->
-#2         11.302768    npadecimientos-> numero de padecimientos
-#3         11.072102           RecUned->Responsabilidad Seguridad en si mismo
-#4         10.457292        EscolMadre->Escolaridad de la madre
-#5         10.370812           AserCog->Cogniciones acerca de la autoeficacia en interacciones sociales
-#6         10.368642        EscolPadre->Escolaridad del padre
-#7         10.107032        RecCarrECA->Recomendación Alguna carrera de la ECA
-#8          9.538026        Climaclima->Clima Primer orden
-#9          8.824725    CreativExpCorp->Empresión Corporal
-#10         8.779562           TecOral-Tecnicas Orales
-
-#3 #Extension of classic CUR: Recalibrating leverages scores
-#and adjusting a mixtures Gaussian models to leverages.
-result <- CUR(data=BaseDatos, variables=AutEficComF1:notabachillerato,
-              k=20, rows = 1, columns = .2, standardize = TRUE,
-              cur_method = "mixture",
-              correlation = R1, correlation_type = "partial")
-
-result$leverage_columns_sorted
-#Se puede observar que se mejoró los resultados y quedó en quinto lugar
-#la escolaridad del padre. Y se mantienen las 10 variables.
-#1         11.812500   SalGasColCabDep
-#2         11.355266    npadecimientos
-#3         11.106225           RecUned
-#4         10.700022        EscolMadre
-#5         10.404541        EscolPadre
-#6         10.372252           AserCog
-#7         10.116369        RecCarrECA
-#8          9.538030        Climaclima
-#9          8.844297    CreativExpCorp
-#10         8.793471           TecOral
-
-
-#7 Relevant variables Plot
-
-result <- CUR(data=BaseDatos, variables=AutEficComF1:notabachillerato,
-              k=20, rows = 1, columns = .2, standardize = TRUE,
-              cur_method = "sample_cur")
-relevant_variables_plot(result)
+# Agrega una línea base
+abline(h = 0)
